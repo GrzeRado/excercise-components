8d5b91d78491d13cbf1efd3c63b21dfe53960254
Grzegorz <rc36@wp.pl>
2018-01-12 11:09:46 +0100

    Excercise components WORKED !!!

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import OtherComponent from './OtherComponent'
+
+const displayText = "ala ma kota"
+
+const App = () => (
+    <div>
+        <OtherComponent
+                text={displayText}
+        />
+    </div>
+
+)
+export default App
---
+++ src/Comp2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Comp2 = (props) => {
+    return <div>{props.text}</div>
+}
+
+export default Comp2
